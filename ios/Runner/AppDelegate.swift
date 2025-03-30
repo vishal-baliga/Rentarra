8e178483ca419494eeb2fe9da76bdcf5902781e1
Vishal Baliga <diztortjelly@Vishals-MacBook-Air.local>
2025-03-29 23:37:34 -0400

    ✅ iOS Firebase setup, onboarding, and fixes for simulator compatibility

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Flutter
+import GoogleMaps
+import FirebaseCore
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +9,16 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    // ✅ Firebase initialization
+    FirebaseApp.configure()
+
+    // ✅ Google Maps initialization (replace with your real key)
+    GMSServices.provideAPIKey("YAIzaSyAi2FoBGhuNMEd1pXwNynU8dJm3jdTlXB4")
+
+    // ✅ Register plugins
     GeneratedPluginRegistrant.register(with: self)
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
